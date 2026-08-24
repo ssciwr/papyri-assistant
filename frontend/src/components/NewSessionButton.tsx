8773bae1c37e2c1e45f81270d7427747121b361b
Harald Mack <harald.mack@protonmail.com>
2026-08-24 10:40:48 +0200

    update new session button to be more indicative of its purpose

--- frontend/src/components/NewSessionButton.tsx
+++ frontend/src/components/NewSessionButton.tsx
@@ -42,21 +42,7 @@ export function NewSessionButton() {
       title="Start new session"
       type="button"
     >
-      <svg
-        aria-hidden="true"
-        fill="none"
-        height="20"
-        viewBox="0 0 24 24"
-        width="20"
-      >
-        <path
-          d="M12 5v14M5 12h14"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-        />
-      </svg>
+      New
     </button>
   );
 }
