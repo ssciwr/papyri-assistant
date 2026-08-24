88cb4139d604f816517d9117213f241796641016
Harald Mack <harald.mack@protonmail.com>
2026-08-24 10:59:42 +0200

    remove sql verifier for now. more trouble than worth

--- backend/src/papyri_backend/verify/sql.py
+++ backend/src/papyri_backend/verify/sql.py
@@ -1,10 +1,11 @@
 def sql_query_verifier(query):
+    # print(query)
     # forbidden
-    if ";" in query.rstrip(";"):
-        raise ValueError("One statement only")
+    # if ";" in query.rstrip(";"):
+    #     raise ValueError("One sql statement only")
 
-    if not query.upper().startswith("SELECT "):
-        raise ValueError("Only SELECT queries are allowed")
+    # if not query.upper().startswith("SELECT "):
+    #     raise ValueError("Only SELECT queries are allowed")
 
     # check syntax validation
     return True
